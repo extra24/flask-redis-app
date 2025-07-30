ee0a45f12a93e2754f884a59a1f895d599fef2c7
Aram Kim <6aram@naver.com>
2025-07-30 16:05:23 +0900

    init

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ r = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():
-    count = r.incr('hits')
+    count = r.incr('hits') 
     return f"Hello! This page has been viewed {count} times."
 
 if __name__ == "__main__":
